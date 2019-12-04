8a20ac36dc80b7a29595d8048ba871304e5cfcf6
andrezafernandes <andrezafernandes@insight-labs-Mac-mini.local>
2019-12-04 13:42:38 -0300

    [+] Validação CPF e redirecionamento pra página dos certificados -> Falta terminar a pag dos certificados

--- front/js/home.js
+++ front/js/home.js
@@ -1,6 +1,39 @@
+function valCpf(value){
+    value = value.replace('.','');
+    value = value.replace('.','');
+    cpf = value.replace('-','');
+    
+    while(cpf.length < 11) cpf = "0"+ cpf;
+    
+    var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+    var a = [];
+    var b = new Number;
+    var c = 11;
+    
+    for (i=0; i<11; i++){
+        a[i] = cpf.charAt(i);
+        if (i < 9) b += (a[i] * --c);
+    }
+    if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11-x }
+    b = 0;
+    c = 11;
+    for (y=0; y<10; y++) b += (a[y] * c--);
+    if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11-x; }
+
+    var retorno = true;
+    if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) retorno = false;
+
+    return retorno;
+}
+
 function buscar_certificados(event){
     // 1. Ver se CPF é valido
-
-    // 2. redirecionar pra pag dos certificados
-    window.location.href = 'pages/certificados.html';
+    var cpf = document.getElementById('cpf').value;
+    // console.log(cpf);
+    if(valCpf(cpf)) {
+        sessionStorage.setItem('cpf', cpf);
+        window.location.href = 'pages/certificados.html';
+    } else {
+        alert("Digite um CPF válido!");
+    }
 }
