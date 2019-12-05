0158de0047bff2b6f8706d7a7463baec3be6275f
ArinaSanches <sanchesarina96@gmail.com>
2019-12-05 00:24:01 -0300

    Correções de erro na pagina participaçãoEvento

--- front/js/participantesEvento.js
+++ front/js/participantesEvento.js
@@ -4,6 +4,34 @@ const $tableParticipacaoID = $('#tableParticipacao');
 var elemento;
 var botao;
 
+function valCpf(value){
+    value = value.replace('.','');
+    value = value.replace('.','');
+    cpf = value.replace('-','');
+    
+    while(cpf.length < 11) cpf = "0"+ cpf;
+    
+    var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+    var a = [];
+    var b = new Number;
+    var c = 11;
+    
+    for (i=0; i<11; i++){
+        a[i] = cpf.charAt(i);
+        if (i < 9) b += (a[i] * --c);
+    }
+    if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11-x }
+    b = 0;
+    c = 11;
+    for (y=0; y<10; y++) b += (a[y] * c--);
+    if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11-x; }
+
+    var retorno = true;
+    if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) retorno = false;
+
+    return retorno;
+}
+
 const newTr = "<tr>" +
         "<td class='pt-3-half texto' contenteditable='true'> </td>" +
         "<td class='pt-3-half numero' contenteditable='false'> </td>" +
@@ -35,28 +63,38 @@ function registrarTexto(){
         elemento = $(this).find('.numero')
 
         if(texto != ' '){ 
-            console.log("ENTROU")
-            $.ajax({
-                headers: { "Accept": "application/json" },
-                type: "POST",
-                async: false,
-                crossDomain: true,
-                url: "http://localhost:3004/api/texto",
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify(send_data),
-                beforeSend: function(xhr) {
-                    xhr.withCredentials = true;
-                    xhr.setRequestHeader('Authorization','Bearer ' + sessionStorage.getItem('token'));
-                },
-                success: function(msg) {
-                    elemento.replaceWith("<td class='pt-3-half numero' contenteditable='false'>"+msg['numero']+"</td>");
+            if((texto.match(/@nome/g) || []).length > 0){
+                if((texto.match(/@cpf/g) || []).length > 0){
+                    if((texto.match(/@horas/g) || []).length > 0){
+                        console.log("ENTROU")
+                        $.ajax({
+                            headers: { "Accept": "application/json" },
+                            type: "POST",
+                            async: false,
+                            crossDomain: true,
+                            url: "http://localhost:3004/api/texto",
+                            contentType: 'application/json',
+                            dataType: 'json',
+                            data: JSON.stringify(send_data),
+                            beforeSend: function(xhr) {
+                                xhr.withCredentials = true;
+                                xhr.setRequestHeader('Authorization','Bearer ' + sessionStorage.getItem('token'));
+                            },
+                            success: function(msg) {
+                                elemento.replaceWith("<td class='pt-3-half numero' contenteditable='false'>"+msg['numero']+"</td>");
+                            }
+                        });
+                    }else{
+                        alert("O texto precisa conter @horas!");    
+                    }
+                }else{
+                    alert("O texto precisa conter @cpf!");    
                 }
-            });
-        }
-    
-    });
-          
+            }else{
+                alert("O texto precisa conter @nome!");    
+            }
+        }    
+    });          
 }
 
 function eliminarTexto(){
@@ -72,24 +110,28 @@ function registrarParticipacao(){
         var nome = $(this).find('.nome').text();
         var cpf = $(this).find('.cpf').text();
         var texto = $(this).find('.texto').text();
-        var ch = $(this).find('.ch').text();   
-        var send_data = {"id_evento": sessionStorage.getItem('id_entidade'), "nome_pessoa": nome, "cpf":cpf, "horas":ch, "texto":texto};
-        $.ajax({
-            headers: { "Accept": "application/json" },
-            type: "POST",
-            crossDomain: true,
-            url: "http://localhost:3004/api/participacao",
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify(send_data),
-            beforeSend: function(xhr) {
-                xhr.withCredentials = true;
-                xhr.setRequestHeader('Authorization','Bearer ' + sessionStorage.getItem('token'));
-            },
-            success: function(msg) {
-                console.log("ok")
-            }
-        });        
+        var ch = $(this).find('.ch').text();  
+        if(valCpf(cpf)) { 
+            var send_data = {"id_evento": sessionStorage.getItem('id_entidade'), "nome_pessoa": nome, "cpf":cpf, "horas":ch, "texto":texto};
+            $.ajax({
+                headers: { "Accept": "application/json" },
+                type: "POST",
+                crossDomain: true,
+                url: "http://localhost:3004/api/participacao",
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(send_data),
+                beforeSend: function(xhr) {
+                    xhr.withCredentials = true;
+                    xhr.setRequestHeader('Authorization','Bearer ' + sessionStorage.getItem('token'));
+                },
+                success: function(msg) {
+                    console.log("ok")
+                }
+            });
+        } else {
+            alert("Digite um CPF válido!");
+        }        
     });    
 }
 
