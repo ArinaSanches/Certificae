d4a103ae9437cdc16ee7320585cee644e1a761a2
andrezafernandes <andrezafernandes@insight-labs-Mac-mini.local>
2019-12-06 14:43:56 -0300

    [+] footer nas pags

--- backend/src/controllers/CertificadoController.js
+++ backend/src/controllers/CertificadoController.js
@@ -9,9 +9,11 @@ module.exports = {
 
         // 1. pega as participacoes dele pelo cpf
         const participacoes = await Participacao.find({"cpf": req.params.cpf});
-
+        const texto = await
         console.log(participacoes)
         // 2. pega os textos dele em Textos
+
+
         // 3. pega os eventos que ele participou em Eventos
 
         // montar dicionario de resposta = 
