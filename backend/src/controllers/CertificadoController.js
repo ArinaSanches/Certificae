297e73203370ff301a1204f09f2b0d5dfff7bde8
andrezafernandes <andrezafernandes@insight-labs-Mac-mini.local>
2019-12-11 17:13:10 -0300

    [+] Ta funcionando pra 1 linha de texto kkkkkk triste

--- backend/src/controllers/CertificadoController.js
+++ backend/src/controllers/CertificadoController.js
@@ -10,49 +10,48 @@ const sharp = require('sharp');
 certificados = [];
 
 async function gerar_certificado(texto, img_base, nome_img){
-    // var textoSVG = ` <svg>
-    //                 <rect x="0" fill="none" y="0" width="800" height="200" rx="50" ry="50"/> `
+    var textoSVG = ` <svg>
+                    <rect x="0" fill="none" y="0" width="800" height="200" rx="50" ry="50"/> `
 
-    // var x = 10,
-    //     y = 40,
-    //     i = 0,
-    //     // interacoes = texto.length / 100;
-    //     style = `style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center"`;
+    var x = 10,
+        y = 40,
+        i = 0,
+        // interacoes = texto.length / 100;
+        style = `style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center"`;
 
     // //TODO: Verificação pra quando for a ultima rodada
     // for(i = 0; i < texto.length; i += 100){
-    //     textoSVG += `<text x=`+ x + ` y=`+ y + ` `+ style + `>`
-    //                     + texto.substr(i, i+=99);
-    //                 +`</text>`;
+    // console.log(texto.substr(i, i+=99));
+        textoSVG += `<text x="10" y="40" style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">` + texto.substr(i, i+=120) +`</text>`
     //     x += 
     // }
 
-    // textoSVG += `</svg>`;
-    // const textoBuffer = Buffer.from( textoSVG);
+    textoSVG += `</svg>`;
+    const textoBuffer = Buffer.from( textoSVG);
 
 
-    const roundedCorners = Buffer.from(
-        `
-            <svg>
-            <rect x="0" fill="none" y="0" width="800" height="200" rx="50" ry="50"/>
-                <text x="10" y="40"  style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">
-                    ESSE aaa É UM CERTIFICADO TOP DEMAIS vamos agora testar até onde vai este carai aqui muito locão aaaa aaaa aaaaa uuuuuuuuu 
-                </text>
+    // const roundedCorners = Buffer.from(
+    //     `
+    //         <svg>
+    //         <rect x="0" fill="none" y="0" width="800" height="200" rx="50" ry="50"/>
+    //             <text x="10" y="40"  style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">
+    //                 ESSE aaa É UM CERTIFICADO TOP DEMAIS vamos agora testar até onde vai este carai aqui muito locão aaaa aaaa aaaaa uuuuuuuuu 
+    //             </text>
     
-                <text x="10" y="80"  style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">
-                    ESSE É UM CERTIFICADO TOP DEMAIS vamos agora testar até onde vai este carai aqui muito locão aaaa aaaa aaaaa
-                </text>
+    //             <text x="10" y="80"  style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">
+    //                 ESSE É UM CERTIFICADO TOP DEMAIS vamos agora testar até onde vai este carai aqui muito locão aaaa aaaa aaaaa
+    //             </text>
     
-                <text x="10" y="120"  style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">
-                    ESSE É UM CERTIFICADO TOP DEMAIS vamos agora testar até onde vai este carai aqui muito locão aaaa aaaa aaaaa
-                 </text>
-            </svg>
-        `
-      );
+    //             <text x="10" y="120"  style="font-family: Times New Roman; font-size: 20; stroke: none; fill: black; text-align: center">
+    //                 ESSE É UM CERTIFICADO TOP DEMAIS vamos agora testar até onde vai este carai aqui muito locão aaaa aaaa aaaaa
+    //              </text>
+    //         </svg>
+    //     `
+    //   );
     
     const img_final = sharp(img_base)
                     .composite([{
-                        input: roundedCorners,
+                        input: textoBuffer,
                         blend: 'atop'
                     }])
                     .toFile(nome_img);
@@ -98,7 +97,7 @@ module.exports = {
             certificados.push(await get_infos(participacao))
         }
         
-        console.log(certificados);
+        // console.log(certificados);
         
         return res.json(certificados);
     },
